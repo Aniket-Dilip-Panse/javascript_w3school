3e886439a2697f36b10676f1f98cb6f5e581b75a
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-03 01:39:29 +0530

    inside function file function Expression and nested function added.

--- JS Tutorial/function.js
+++ JS Tutorial/function.js
@@ -15,3 +15,27 @@ function newFunction(name, age, address) {
 }
 
 newFunction('aniket', 20, 'mumbai')
+
+//function Expression:- It is same as a function declaration but the main difference it is assigned to a variable.
+
+// ===============IMPORTANT============
+// | In function Expression we can omit the function name|
+
+// In function Expression hoisting is not possible.it will produce an error.
+const result = function(){
+    console.log('hello');
+}
+result()
+
+
+
+// nested function in javascript most reactjs code is written in nested function.
+
+function myFunction() {
+    console.log('namaskar');
+    function newFunction(name, age, address) {
+        console.log(name, age, address);
+    }
+    newFunction('aniket', 20, 'mumbai')
+}
+myFunction()
