6a2bbf25729ba11ede8036515858ffdb7779e928
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-02 21:48:30 +0530

    javacript string.js file added

---
+++ JS Tutorial/strings.js
@@ -0,0 +1,20 @@
+// strings in javascript 
+let a = "hello"
+let b = "world"
+console.log(a + ' ' + b)
+
+// Template Strings
+// Templates were introduced with ES6 (JavaScript 2016).
+
+// Templates are strings enclosed in backticks (`This is a template string`).
+
+// Templates allow single and double quotes inside a string:
+let value = `The sum of 2 and 3 is ${2 + 3}`;
+console.log(value)
+
+// String Length
+console.log(value.length)
+
+// string == and ===
+console.log(a == b) //true
+console.log(a === b) // false objects cannot be compared in javascript.
