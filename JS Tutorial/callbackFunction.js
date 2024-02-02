91ed37878bc1951da8383551ab64d09213dec5c1
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-03 02:04:10 +0530

    new seperate callbackFunction.js file added

---
+++ JS Tutorial/callbackFunction.js
@@ -0,0 +1,13 @@
+// A callback is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
+
+// we don't have to call the function into another function we need to just pass the function into another function.
+
+const calculate = (a,b,operation) => { 
+    return operation(a, b);
+}
+
+calculate(5, 5, (a, b) => a + b)
+const result = calculate(5, 5, function (num1, num2) {
+    return num1 + num2
+})
+console.log(result)
