24a38cb9d4bc53d84a3ad1b70f7305d455522940
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-03 01:53:03 +0530

    arrow function code added

--- JS Tutorial/function.js
+++ JS Tutorial/function.js
@@ -39,3 +39,35 @@ function myFunction() {
     newFunction('aniket', 20, 'mumbai')
 }
 myFunction()
+
+
+// ADVANCED FUNCTION IN JAVASCRIPT
+// 1) Arrow Function
+// 2) Callback Function // most of the time used
+
+// 1) Arrow Function.
+
+
+// syntax 1):-
+/* 
+   () => {
+    statement
+     }
+*/
+
+
+// syntax 2):-
+//     () => expression
+
+// syntax 3):-
+//     (para1,para2,para3) => statement
+const greet = (count) => {
+    for (let i = 0; i < count; i++) {
+        console.log(`${i}`);
+    }
+    console.log('namaskar');
+}
+greet(4);
+
+const square = (x) => x * x
+console.log(square(5))
