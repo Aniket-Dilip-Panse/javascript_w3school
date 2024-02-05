b6f6ef72e117121128f27e2f4b883df493a18f55
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-05 19:00:10 +0530

    new file stringMethod.js added

---
+++ JS Tutorial/stringMethod.js
@@ -0,0 +1,11 @@
+let name = 'aniket dilip panse'
+let email = 'aniket12@gmail.com'
+let address = 'Mumbai'
+
+let info = {
+    'name': `${name}`,
+    'email': `${email}`,
+    'address': `${address}`
+}
+
+console.log(info);
