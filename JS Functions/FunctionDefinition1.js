ba7f2f529f0c9479d4a29f8440e7ce055e338849
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-06 18:04:52 +0530

    functiondefinition file code change

--- JS Functions/FunctionDefinition1.js
+++ JS Functions/FunctionDefinition1.js
@@ -1,3 +1,20 @@
 // JavaScript functions are defined with the function keyword.
 
 // You can use a function declaration or a function expression.
+
+// 1)function declaration:- The function is not stored in variable.
+function myFunction() {
+    console.log('hello');
+}
+
+// 2)function expression:- The function is stored in variable.The variable can be used as a function.
+
+// The function above is actually an anonymous function (a function without a name).
+
+// The function above ends with a semicolon because it is a part of an executable statement.
+const myFunction = function (a, b) {
+    console.log('hello');
+    console.log(a + b);
+};
+let sum = myFunction(10, 20)
+console.log(sum)
