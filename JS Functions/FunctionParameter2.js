0a6dc83ac46eebee8d3047cc7bad2c7b70cee74a
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-08 23:17:02 +0530

    FunctionParameter.js

--- JS Functions/FunctionParameter2.js
+++ JS Functions/FunctionParameter2.js
@@ -1,1 +1,9 @@
 // A JavaScript function does not perform any checking on parameter values (arguments).
+
+// function functionName(parameter1, parameter2, parameter3) {
+//     // code to be executed
+//   }
+
+// Function parameters are the names listed in the function definition.
+
+// Function arguments are the real values passed to (and received by) the function.
