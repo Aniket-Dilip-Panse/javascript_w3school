f330686faace926fbf7ae3864c1c9ca2a756f891
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-22 14:04:19 +0530

    above word is changed into below

--- JS Functions/FunctionDefinition1.js
+++ JS Functions/FunctionDefinition1.js
@@ -1,6 +1,6 @@
 // JavaScript functions are defined with the function keyword.
 
-// You can use a function declaration or a function expression.
+// You can use a function dec laration or a function expression.
 
 // 1)function declaration:- The function is not stored in variable.
 function myFunction() {
@@ -9,9 +9,9 @@ function myFunction() {
 
 // 2)function expression:- The function is stored in variable.The variable can be used as a function.
 
-// The function above is actually an anonymous function (a function without a name).
+// The function below is actually an anonymous function (a function without a name).
 
-// The function above ends with a semicolon because it is a part of an executable statement.
+// The function below ends with a semicolon because it is a part of an executable statement.
 const myFunction = function (a, b) {
     console.log('hello');
     console.log(a + b);
