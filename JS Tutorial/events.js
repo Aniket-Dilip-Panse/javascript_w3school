06f96474a76c0a2c152fe7da1a50cb0d799c8a5a
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-27 00:45:59 +0530

    different ways to add event in javascript

--- JS Tutorial/events.js
+++ JS Tutorial/events.js
@@ -1,1 +1,19 @@
 // event in js like onclick onmouseover etc.
+
+// method 1 to write direct events
+// {/* <input type="submit" onclick="alert('Hello World')"> */}
+
+
+// method 2 using function call
+// <input type="submit" onclick="myFunction()">
+function myFunction() {
+    alert('Hello World');
+}
+
+// method 3 using arrow function
+// <input type="submit" onclick={() => alert('Hello World')}>
+
+// method 4 using eventListener
+/* <input type="submit" id="myBtn">
+let eventbtn = document.getElementById('myBtn');
+eventbtn.addEventListener('click', () => alert('Hello World')) */
