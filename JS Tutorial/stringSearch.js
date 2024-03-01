46643c2f6c33b08ea389db1c2c1dcc64ef5fcebf
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-03-01 23:38:28 +0530

    new string searching file

---
+++ JS Tutorial/stringSearch.js
@@ -0,0 +1,13 @@
+// javascript search string
+let name = 'aniket dilip panse'
+let email = 'aniket12@gmail.com'
+let address = 'Mumbai'
+
+let info = {
+    'name': `${name}`,
+    'email': `${email}`,
+    'address': `${address}`
+}
+
+let finded = name.search("/aniket/gwi");
+console.log(finded)
